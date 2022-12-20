3467ba72f268e875cbb71003d34e26482d8f042a
Nishkarsh-Jain <94288727+Nishkarsh-Jain@users.noreply.github.com>
2022-12-20 20:23:21 +0530

    Animated Login Page done

---
+++ src/app/animated-login-form/animated-login-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-animated-login-form',
+  templateUrl: './animated-login-form.component.html',
+  styleUrls: ['./animated-login-form.component.css']
+})
+export class AnimatedLoginFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
