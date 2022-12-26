d449afb7ca3f97711d6674c622a9cb04b7e75268
Nishkarsh-Jain <94288727+Nishkarsh-Jain@users.noreply.github.com>
2022-12-26 17:39:41 +0530

    Development

---
+++ src/app/animated-login-form2/animated-login-form2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-animated-login-form2',
+  templateUrl: './animated-login-form2.component.html',
+  styleUrls: ['./animated-login-form2.component.css']
+})
+export class AnimatedLoginForm2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AnimatedLoginFormComponent } from './animated-login-form/animated-login-form.component';
+import { AnimatedLoginForm2Component } from './animated-login-form2/animated-login-form2.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, AnimatedLoginFormComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    AnimatedLoginFormComponent,
+    AnimatedLoginForm2Component,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
